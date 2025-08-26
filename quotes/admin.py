9b59b4213c2986d3d5a50c0bbde6c816b879ed23
Veanyk <daniilkunaev0010@gmail.com>
2025-08-26 21:57:15 +0700

    update v.0.982

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from .models import Quote, Source, Vote
 
-@admin.register(Quote)
 class QuoteAdmin(admin.ModelAdmin):
-    list_display = ('text', 'source', 'is_active', 'created_at')
+    list_display = (
+        'text',
+        'source',
+        'weight',
+        'views',
+        'likes',
+        'dislikes',
+        'is_active',
+        'created_at'
+    )
+
     list_filter = ('is_active', 'source__kind')
     search_fields = ('text', 'source__name')
-    list_editable = ('is_active',)
+    list_editable = ('weight', 'is_active',)
+    ordering = ('-created_at',)
     actions = ['approve_quotes']
 
     @admin.action(description='Одобрить выбранные цитаты')
